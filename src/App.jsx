ecefc5cc0e45778d3356ed04c4b3cb175d3d9bd1
Carlo-Falanga <carlofalanga7@gmail.com>
2026-05-18 16:34:46 +0200

    style: wrap main content in a div with pageWrap class for improved layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import SiteHeader from "./components/Header/SiteHeader";
 import SiteFooter from "./components/Footer/SiteFooter";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <>
-      <SiteHeader />
-      <div>Main Page</div>
-      <SiteFooter />
+      <div className={styles.pageWrap}>
+        <SiteHeader />
+        <div>Main Page</div>
+        <SiteFooter />
+      </div>
     </>
   );
 }
